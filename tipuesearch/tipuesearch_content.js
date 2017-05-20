9456eaa61da46c5ae3a2e4350229dd9110b21059
PatrickA7 <patrick.alegria7@gmail.com>
2017-05-21 01:16:01 +0930

    Added search function

---
+++ tipuesearch/tipuesearch_content.js
@@ -0,0 +1,12 @@
+
+var tipuesearch = {"pages": [
+     {"title": "Home", "text": "CDU IT Code Fair is a coding and design competition for university students.", "tags": "home code fair ", "url": "https://patricka7.github.io/code_fair/"},
+     {"title": "About Us", "text": "The CDU IT Code Fair was created to give a platform for talented university students who are aspiring designers, creators, innovators, and artists to showcase their work. Ever since it started in insert date the growth of the event has has been enormous to get the sponsorship of the NT Government and major IT companies in Australia such as the Australia Computer Society, NEC, Kinetic IT and more. For 2017 we hope the event will be bigger than ever and we hope to the creations that people up with!", "tags": "about us info information date dates prize prizes location staff sponsors judges rules sign up ", "url": "https://patricka7.github.io/code_fair/about.html"},
+    {"title": "Recent News", "text": "Recent news regarding the CDU 2017 Code Fair. Catch up on the latest news about the Code Fair and it's events!", "tags": "recent news code fair employer speed dating ", "url": "https://patricka7.github.io/code_fair/news.html"},
+    {"title": "Schedule", "text": "Find times, dates, and locations for events being run during the 2017 Code Fair", "tags": "schedule time date timetable event list ", "url": "https://patricka7.github.io/code_fair/schedule.html"},
+    {"title": "Sign Up", "text": "Sign up for competitions and events", "tags": "sign up event enter speed dating ", "url": "https://patricka7.github.io/code_fair/signup.html"},
+    {"title": "Contact", "text": "Contact event organisers and supervisors for more information", "tags": "support contact help ", "url": "https://patricka7.github.io/code_fair/contact.html"}
+]};
+
+
+
